695b4a2b630a18cda8914f5e1fd0010eef0e66d1
roman <romanvanloo@icloud.com>
2021-02-06 18:17:32 +0100

    try first page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,3 +1,16 @@
 class PagesController < ApplicationController
-  def index; end
+  def index
+  end
+
+  def impressions
+  end
+
+  def before_and_after
+  end
+
+  def at_work
+  end
+
+  def about
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root "pages#index"
+  root "pages#impressions"
+  get "before_and_after", to: "pages#before_and_after"
+  get "at_work", to: "pages#at_work"
+  get "about", to: "pages#about"
 end
